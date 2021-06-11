4db24103fe7855dffed9d4550961912a8f498ee9
Ben Sepanski <ben.sepanski@gmail.com>
2021-06-11 08:59:58 -0700

    malloc zCoeff

--- single/cpu.cpp
+++ single/cpu.cpp
@@ -10,6 +10,7 @@ bComplexElem *zCoeff;
 
 int main() {
   coeff = (bElem *) malloc(129 * sizeof(bElem));
+  zCoeff = (bComplexElem *) malloc(129 * sizeof(bComplexElem));
   std::random_device r;
   std::mt19937_64 mt(r());
   std::uniform_real_distribution<bElem> u(0, 1);
