aace2363dfaae596f4af90ea6dd4a97ced10e5cd
Ben Sepanski <ben.sepanski@gmail.com>
2021-06-18 09:14:57 -0700

    get an example which doesn't error on gtensor

---
+++ stencils/gene-5d-main.cpp
@@ -0,0 +1,57 @@
+#include "gene-5d.h"
+
+template<typename Space>
+using gtensor5D = gt::gtensor<gt::complex<bElem>, 5, Space>;
+
+void ij_deriv_gtensor(bComplexElem *in_ptr, bComplexElem *out_ptr)
+{
+  auto shape = gt::shape(PADDED_EXTENT);
+  // copy in-array to gtensor
+  gtensor5D<gt::space::host> gt_in(shape);
+  complexArray5D in_arr = (complexArray5D) in_ptr;
+  _TILEFOR5D gt_in(i, j, k, l, m) = reinterpret_cast<const gt::complex<bElem>&>(in_arr[m][l][k][j][i]);
+
+  // copy the in-array to device
+  auto gt_in_dev = gt::empty_device<gt::complex<bElem> >(shape);
+  gt::copy(gt_in, gt_in_dev);
+
+  // compute our stencil
+  gtensor5D<gt::space::device> gt_out_dev(shape);
+  gt_out_dev = gt::eval(2.0 * gt_in_dev);
+
+  // copy output data back to host
+  auto gt_out = gt::empty_like(gt_in);
+  gt::copy(gt_out_dev, gt_out);
+
+  // copy host to out_ptr
+  complexArray5D out_arr = (complexArray5D) out_ptr;
+  _TILEFOR5D out_arr[m][l][k][j][i] = reinterpret_cast<const bComplexElem&>(gt_out(i, j, k, l, m));
+}
+
+/**
+ * @brief 1-D stencil fused with multiplication by 1-D array
+ * 
+ * Based on https://github.com/wdmapp/gtensor/blob/d07000b15d253cdeb44942b52f3d2caf4522faa0/benchmarks/ij_deriv.cxx
+ */
+void ij_deriv() {
+    // build in/out arrays
+    bComplexElem *in_ptr = randomComplexArray({PADDED_EXTENT});
+    bComplexElem *out_ptr = zeroComplexArray({PADDED_EXTENT});
+
+    // run computations
+    std::cout << "ij_deriv" << std::endl;
+    ij_deriv_gtensor(in_ptr, out_ptr);
+    std::cout << "done" << std::endl;
+    std::cout << out_ptr[0] << " = 2.0 * " << in_ptr[0] << std::endl;
+
+    free(out_ptr);
+    free(in_ptr);
+}
+
+int main() {
+  // std::random_device r;
+  // std::mt19937_64 mt(r());
+  // std::uniform_real_distribution<bElem> u(0, 1);
+  ij_deriv();
+  return 0;
+}
---
+++ stencils/gene-5d.h
@@ -0,0 +1,97 @@
+#ifndef BRICK_GENE_5D_H
+#define BRICK_GENE_5D_H
+
+#include <algorithm>
+#include <cuComplex.h>
+#include <gtensor/gtensor.h>
+#include <iostream>
+#include <random>
+#include "bricksetup.h"
+#include "multiarray.h"
+#include "brickcompare.h"
+#include "gpuvfold.h"
+#include "stencils_cu.h"
+
+#undef N
+#undef TILE
+#undef GZ
+#undef PADDING
+#undef STRIDE
+#undef STRIDEG
+#undef NB
+#undef GB
+#undef STRIDEB
+#undef BDIM
+#undef VFOLD
+#undef _TILEFOR
+
+// tiling
+#define _TILEFOR5D _Pragma("omp parallel for collapse(4)") \
+for (long tm = PADDING_m; tm < PADDING_m + EXTENT_m; tm += TILE) \
+for (long tl = PADDING_l; tl < PADDING_l + EXTENT_l; tl += TILE) \
+for (long tk = PADDING_k; tk < PADDING_k + EXTENT_k; tk += TILE) \
+for (long tj = PADDING_j; tj < PADDING_j + EXTENT_j; tj += TILE) \
+for (long ti = PADDING_i; ti < PADDING_i + EXTENT_i; ti += TILE) \
+for (long m = tm; m < tm + TILE; ++m) \
+for (long l = tl; l < tl + TILE; ++l) \
+for (long k = tk; k < tk + TILE; ++k) \
+for (long j = tj; j < tj + TILE; ++j) \
+_Pragma("omp simd") \
+for (long i = ti; i < ti + TILE; ++i)
+
+// useful constants
+constexpr bElem pi = 3.14159265358979323846;
+
+#define PADDING_i 8
+#define PADDING_j 8
+#define PADDING_k 8
+#define PADDING_l 8
+#define PADDING_m 8
+#define PADDING PADDING_i,PADDING_j,PADDING_k,PADDING_l,PADDING_m
+#define GHOST_ZONE_i 0
+#define GHOST_ZONE_j 0
+#define GHOST_ZONE_k 0
+#define GHOST_ZONE_l 0
+#define GHOST_ZONE_m 0
+#define GHOST_ZONE GHOST_ZONE_i,GHOST_ZONE_j,GHOST_ZONE_k,GHOST_ZONE_l,GHOST_ZONE_m
+
+// blocking dimensions
+constexpr unsigned DIM = 5;
+constexpr unsigned TILE = 8;
+#define BDIM_i 4
+#define BDIM_j 4
+#define BDIM_k 4
+#define BDIM_l 4
+#define BDIM_m 4
+#define BDIM BDIM_i,BDIM_j,BDIM_k,BDIM_l,BDIM_m
+#define VFOLD 2,4,4
+// num elements in each direction
+#define EXTENT_i 72
+#define EXTENT_j 32
+#define EXTENT_k 24
+#define EXTENT_l 24
+#define EXTENT_m 32
+#define EXTENT EXTENT_i,EXTENT_j,EXTENT_k,EXTENT_l,EXTENT_m
+#define NUM_ELEMENTS EXTENT_i * EXTENT_j * EXTENT_k * EXTENT_l * EXTENT_m
+// num elements in each direction + padding + ghosts
+#define PADDED_EXTENT_i EXTENT_i + 2 * (PADDING_i + GHOST_ZONE_i)
+#define PADDED_EXTENT_j EXTENT_j + 2 * (PADDING_j + GHOST_ZONE_j)
+#define PADDED_EXTENT_k EXTENT_k + 2 * (PADDING_k + GHOST_ZONE_k)
+#define PADDED_EXTENT_l EXTENT_l + 2 * (PADDING_l + GHOST_ZONE_l)
+#define PADDED_EXTENT_m EXTENT_m + 2 * (PADDING_m + GHOST_ZONE_m)
+#define PADDED_EXTENT PADDED_EXTENT_i,PADDED_EXTENT_j,PADDED_EXTENT_k,PADDED_EXTENT_l,PADDED_EXTENT_m
+#define NUM_PADDED_ELEMENTS PADDED_EXTENT_i * PADDED_EXTENT_j * PADDED_EXTENT_k * PADDED_EXTENT_l * PADDED_EXTENT_m
+// number of blocks (including padded blocks)
+#define BRICK_GRID_EXTENT_i (EXTENT_i + 2 * GHOST_ZONE_i) / BDIM_i
+#define BRICK_GRID_EXTENT_j (EXTENT_j + 2 * GHOST_ZONE_j) / BDIM_j
+#define BRICK_GRID_EXTENT_k (EXTENT_k + 2 * GHOST_ZONE_k) / BDIM_k
+#define BRICK_GRID_EXTENT_l (EXTENT_l + 2 * GHOST_ZONE_l) / BDIM_l
+#define BRICK_GRID_EXTENT_m (EXTENT_m + 2 * GHOST_ZONE_m) / BDIM_m
+#define BRICK_GRID_EXTENT BRICK_GRID_EXTENT_i,BRICK_GRID_EXTENT_j,BRICK_GRID_EXTENT_k,BRICK_GRID_EXTENT_l,BRICK_GRID_EXTENT_m
+#define NUM_BRICKS BRICK_GRID_EXTENT_i * BRICK_GRID_EXTENT_j * BRICK_GRID_EXTENT_k * BRICK_GRID_EXTENT_l * BRICK_GRID_EXTENT_m
+
+void cudaDouble(gt::complex<bElem> *inPtr, gt::complex<bElem> *outPtr);
+
+using complexArray5D = bComplexElem(*) [EXTENT_l][EXTENT_k][EXTENT_j][EXTENT_i];
+
+#endif // BRICK_GENE_5D_H
