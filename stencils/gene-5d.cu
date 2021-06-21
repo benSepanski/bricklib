73fbfcc8061a3bd0ba2b7d08ce32b468a2d73823
Ben Sepanski <ben.sepanski@gmail.com>
2021-06-21 13:06:03 -0700

    fix dimension ordering

--- stencils/gene-5d.cu
+++ stencils/gene-5d.cu
@@ -17,25 +17,27 @@ ij_deriv_brick_kernel(unsigned (*fieldGrid)[BRICK_EXTENT_l][BRICK_EXTENT_k][BRIC
                       bElem *i_deriv_coeff) 
 {
   // compute indices
-  long tm = GB_m + blockIdx.z / (BRICK_EXTENT_m * BRICK_EXTENT_l);
-  long tl = GB_l + (blockIdx.z % BRICK_EXTENT_m) / BRICK_EXTENT_l;
-  long tk = GB_k + blockIdx.z % (BRICK_EXTENT_l * BRICK_EXTENT_m);
+  long tm = GB_m + blockIdx.z / (BRICK_EXTENT_k * BRICK_EXTENT_l);
+  long tl = GB_l + (blockIdx.z / BRICK_EXTENT_k) % BRICK_EXTENT_l;
+  long tk = GB_k + blockIdx.z % BRICK_EXTENT_k;
   long tj = GB_j + blockIdx.y;
   long ti = GB_i + blockIdx.x;
-  long m = threadIdx.z / (BDIM_l * BDIM_m);
-  long l = (threadIdx.z % BDIM_m) / BDIM_l;
-  long k = threadIdx.z % (BDIM_l * BDIM_m);
+  long m = threadIdx.z / (BDIM_k * BDIM_l);
+  long l = (threadIdx.z / BDIM_k) % BDIM_l;
+  long k = threadIdx.z % BDIM_k;
   long j = threadIdx.y;
   long i = threadIdx.x;
   unsigned bFieldIndex = fieldGrid[tm][tl][tk][tj][ti];
   unsigned bCoeffIndex = coeffGrid[tm][tl][tk][ti];
   // perform computation
-  bOut[bFieldIndex][m][l][k][j][i] = bP1[bCoeffIndex][m][l][k][i] * (
-    i_deriv_coeff[0] * bIn[bFieldIndex][m][l][k][j][i - 2] +
-    i_deriv_coeff[1] * bIn[bFieldIndex][m][l][k][j][i - 1] +
-    i_deriv_coeff[2] * bIn[bFieldIndex][m][l][k][j][i + 0] +
-    i_deriv_coeff[3] * bIn[bFieldIndex][m][l][k][j][i + 1] +
-    i_deriv_coeff[4] * bIn[bFieldIndex][m][l][k][j][i + 2]
-  ) + 
-  bP2[bCoeffIndex][m][l][k][i] * ikj[j] * bIn[bFieldIndex][m][l][k][j][i];
+  bOut[bFieldIndex][m][l][k][j][i] = bIn[bFieldIndex][m][l][k][j][i];
+  
+  // bP1[bCoeffIndex][m][l][k][i] * (
+  //   i_deriv_coeff[0] * bIn[bFieldIndex][m][l][k][j][i - 2] +
+  //   i_deriv_coeff[1] * bIn[bFieldIndex][m][l][k][j][i - 1] +
+  //   i_deriv_coeff[2] * bIn[bFieldIndex][m][l][k][j][i + 0] +
+  //   i_deriv_coeff[3] * bIn[bFieldIndex][m][l][k][j][i + 1] +
+  //   i_deriv_coeff[4] * bIn[bFieldIndex][m][l][k][j][i + 2]
+  // ) + 
+  // bP2[bCoeffIndex][m][l][k][i] * ikj[PADDING_j + tj * BDIM_j + j] * bIn[bFieldIndex][m][l][k][j][i];
 }
