34b307e158ae384f6705f299d65b991df83f02af
Ben Sepanski <ben.sepanski@gmail.com>
2021-07-27 11:55:24 -0700

    switch to 1D transpose and set up brick trans-fft-trans

--- single/fft-gene-6d.cu
+++ single/fft-gene-6d.cu
@@ -123,6 +123,7 @@ double complex_to_complex_1d_j_fft_array(bComplexElem *in_arr, bComplexElem *out
   // free memory
   cudaCheck(cudaFree(out_arr_dev));
   cudaCheck(cudaFree(in_arr_dev));
+  cufftCheck(cufftDestroy(plan));
 
   // return timing
   return num_seconds;
@@ -158,7 +159,6 @@ double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComple
                            embed, stride, distBetweenBatches,
                            type, numBatches));
 
-  for(size_t i = 0; i < NUM_ELEMENTS; ++i) in_arr[i] = bComplexElem((double) i);
   // copy data to device
   bCuComplexElem *in_arr_dev, *out_arr_dev, *intermed_arr_dev;
   constexpr size_t ARR_SIZE = sizeof(bComplexElem) * NUM_ELEMENTS;
@@ -170,25 +170,17 @@ double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComple
   // lambda function for timing
   auto compute_fft = [&plan](const bCuComplexElem *in_arr, bCuComplexElem *intermed_arr, bCuComplexElem *out_arr, int direction) -> void 
   {
-    constexpr int blockSize = 256;
-    constexpr unsigned TileJ = 32;
-    constexpr unsigned TileI = blockSize / TileJ;
-    dim3 dimBlock(TileI, TileJ),
-         dimGrid(EXTENT_i / dimBlock.x,
-                 EXTENT_j / dimBlock.y,
-                 NUM_ELEMENTS / EXTENT_i / EXTENT_j / dimBlock.z);
-    static_assert(EXTENT_i % TileI == 0);
-    static_assert(EXTENT_j % TileJ == 0);
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    int blockSize = 128;
+    int numBlocks = prop.multiProcessorCount * (prop.maxThreadsPerMultiProcessor / blockSize) * 4;
     // transpose in -> out 
-    transpose_ij<TileJ, TileI><< <dimGrid, dimBlock>> >(in_arr, out_arr, EXTENT_j, EXTENT_i);
-    cudaDeviceSynchronize();
-    // // fft out -> intermed
+    constexpr unsigned TileJ = 32, TileI = 8;
+    transpose_ij<TileJ, TileI><< <numBlocks, blockSize>> >(in_arr, out_arr, NUM_ELEMENTS / EXTENT_j / EXTENT_i, EXTENT_j, EXTENT_i);
+    // fft out -> intermed
     cufftCheck(cufftXtExec(plan, out_arr, intermed_arr, direction));
     // transpose intermed -> out
-    dim3 dimBlockT(dimBlock.y, dimBlock.x, dimBlock.z),
-         dimGridT(dimGrid.y, dimGrid.x, dimGrid.z);
-    transpose_ij<TileI, TileJ><< <dimGridT, dimBlockT>> >(intermed_arr, out_arr, EXTENT_i, EXTENT_j);
-    cudaDeviceSynchronize();
+    transpose_ij<TileI, TileJ><< <numBlocks, blockSize>> >(intermed_arr, out_arr, NUM_ELEMENTS / EXTENT_j / EXTENT_i, EXTENT_i, EXTENT_j);
   };
   // time function (and compute fft in process)
   double num_seconds = cutime_func([&compute_fft, &in_arr_dev, &intermed_arr_dev, &out_arr_dev, &direction]() -> void {
@@ -223,6 +215,7 @@ double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComple
   cudaCheck(cudaFree(out_arr_dev));
   cudaCheck(cudaFree(intermed_arr_dev));
   cudaCheck(cudaFree(in_arr_dev));
+  cufftCheck(cufftDestroy(plan));
 
   // return timing
   return num_seconds;
@@ -325,6 +318,151 @@ double complex_to_complex_1d_j_fft_brick(bComplexElem *in_arr, bComplexElem *out
   return num_seconds;
 }
 
+namespace { // anonymous namespace
+  constexpr int gcd(int a, int b) {
+    if (b == 0)
+    return a;
+    return gcd(b, a % b);
+  }
+} // anonymous namespace
+
+/**
+ * @brief time computation of 1D FFT in j-dimension of in_arr into out_arr
+ * 
+ * Uses bricks data layout with a transpose before/after
+ * 
+ * @param in_arr[in] host-side input array, of rank DIM with extents EXTENT_n,...,EXTENT_i
+ * @param out_arr[out] host-side output array of same shape as in_arr
+ * @param direction[in] either CUFFT_FORWARD or CUFFT_INVERSE
+ * @param warmup number of warmup iterations
+ * @param iter number of iterations
+ * @return the average number of seconds to compute the FFT
+ */
+double complex_to_complex_1d_j_fft_brick_transpose(bComplexElem *in_arr, bComplexElem *out_arr, int warmup, int iter, int direction = CUFFT_FORWARD)
+{
+  if(direction != CUFFT_FORWARD && direction != CUFFT_INVERSE) throw std::runtime_error("Unrecognized direction");
+
+  // useful typedefs for brick
+  typedef Dim<BDIM_n,BDIM_m,BDIM_l,BDIM_k,BDIM_j,BDIM_i> BrickDims;
+  typedef Dim<BDIM_n,BDIM_m,BDIM_l,BDIM_k,BDIM_i,BDIM_j> BrickDimsTransposed;
+  typedef Dim<1> VFold;
+  constexpr bool isComplex = true;
+  typedef CommDims<false,false,false,false,false,false> NoComm;
+  typedef Brick<BrickDims, VFold, isComplex, NoComm> ComplexBrick;
+  typedef Brick<BrickDimsTransposed, VFold, isComplex, NoComm> ComplexBrickTransposed;
+
+  // move arrays to host-side bricks
+  unsigned *grid_ptr = nullptr;
+  BrickInfo<DIM, NoComm> bInfo = init_grid<DIM, NoComm>(grid_ptr, {BRICK_EXTENT});
+  BrickStorage bInStorage = bInfo.allocate(ComplexBrick::BRICKSIZE),
+               bOutStorage = bInfo.allocate(ComplexBrick::BRICKSIZE);
+  ComplexBrick bIn(&bInfo, bInStorage, 0),
+               bOut(&bInfo, bOutStorage, 0);
+  copyToBrick<DIM>({EXTENT}, std::vector<long>(DIM, 0), std::vector<long>(DIM, 0), in_arr, grid_ptr, bIn);
+
+  // set up brick info in cuda
+  BrickInfo<DIM, NoComm> *bInfo_dev;
+  BrickInfo<DIM, NoComm> _bInfo_dev = movBrickInfo(bInfo, cudaMemcpyHostToDevice);
+  cudaCheck(cudaMalloc(&bInfo_dev, sizeof(BrickInfo<DIM, NoComm>)));
+  cudaCheck(cudaMemcpy(bInfo_dev, &_bInfo_dev, sizeof(BrickInfo<DIM, NoComm>), cudaMemcpyHostToDevice));
+  // mov brick storage to cuda
+  BrickStorage bInStorage_dev = movBrickStorage(bInStorage, cudaMemcpyHostToDevice);
+  BrickStorage bOutStorage_dev = movBrickStorage(bOutStorage, cudaMemcpyHostToDevice);
+  // set up brick in cuda
+  ComplexBrick bIn_dev(bInfo_dev, bInStorage_dev, 0);
+  ComplexBrick bOut_dev(bInfo_dev, bOutStorage_dev, 0);
+  // copy grids to device
+  unsigned *grid_ptr_dev;
+  size_t size = sizeof(unsigned) * NUM_BRICKS;
+  cudaCheck(cudaMalloc(&grid_ptr_dev, size));
+  cudaCheck(cudaMemcpy(grid_ptr_dev, grid_ptr, size, cudaMemcpyHostToDevice));
+
+  // set up brickinfo for transposed bricks
+  BrickInfo<DIM, NoComm> _bInfoTransposed_dev = movBrickInfo(bInfo, cudaMemcpyHostToDevice);
+  BrickInfo<DIM, NoComm> *bInfoTransposed_dev;
+  cudaCheck(cudaMalloc(&bInfoTransposed_dev, sizeof(BrickInfo<DIM, NoComm>)));
+  cudaCheck(cudaMemcpy(bInfoTransposed_dev, &_bInfoTransposed_dev, sizeof(BrickInfo<DIM, NoComm>), cudaMemcpyHostToDevice));
+  // set up transposed grid ptr
+  unsigned *grid_ptr_transposed_dev;
+  cudaCheck(cudaMalloc(&grid_ptr_transposed_dev, size));
+  transpose_brick_info_ij_on_device(bInfo.nbricks, 64, bInfo_dev, bInfoTransposed_dev);
+  constexpr size_t CollapsedDims = BRICK_EXTENT_n * BRICK_EXTENT_m * BRICK_EXTENT_l * BRICK_EXTENT_k;
+  constexpr unsigned TileJ = 32, TileI = 8;
+  cudaDeviceProp prop;
+  cudaGetDeviceProperties(&prop, 0);
+  int blockSize = 128;
+  int numBlocks = prop.multiProcessorCount * (prop.maxThreadsPerMultiProcessor / blockSize) * 4;
+  transpose_ij<TileI, TileJ><< <numBlocks, blockSize>> >(grid_ptr_dev, grid_ptr_transposed_dev, CollapsedDims, BRICK_EXTENT_i, BRICK_EXTENT_j);
+  cudaCheck(cudaDeviceSynchronize());
+  cudaCheck(cudaPeekAtLastError());
+
+  // set up intermediate brick on device
+  BrickStorage intermedStorage_dev = movBrickStorage(bInStorage, cudaMemcpyHostToDevice);
+  ComplexBrickTransposed bIntermed_dev(bInfoTransposed_dev, intermedStorage_dev, 0);
+
+  // set up FFT in 0-dimensional for bricks
+  typedef BricksCufftPlan<ComplexBrickTransposed, FourierType<ComplexToComplex, 0> > FFTPlanType;
+  std::array<size_t, DIM> transposed_grid_size = {BRICK_EXTENT_j, BRICK_EXTENT_i, BRICK_EXTENT_k,
+                                                  BRICK_EXTENT_l, BRICK_EXTENT_m, BRICK_EXTENT_n};
+  FFTPlanType plan(transposed_grid_size);
+  plan.setup(bIntermed_dev, grid_ptr_transposed_dev, bIntermed_dev, grid_ptr_transposed_dev);
+  auto compute_fft = [&plan, &bInfo, &bIn_dev, &bIntermed_dev, &bOut_dev](bool direction = FFTPlanType::BRICKS_FFT_FORWARD) -> void {
+    // transpose into intermediate brick
+    transpose_brick_ij<< <bInfo.nbricks, 64>> >(bIn_dev, bIntermed_dev);
+    // // compute FFT on intermed-brick
+    plan.launch(direction); 
+    // transpose intermed back into tou-brick
+    transpose_brick_ij<< <bInfo.nbricks, 64>> >(bIntermed_dev, bOut_dev);
+  };
+  // time function (and compute fft in process)
+  double num_seconds = cutime_func(compute_fft, warmup, iter);
+
+  // copy data back from device
+  cudaCheck(cudaMemcpy(bOutStorage.dat.get(), bOutStorage_dev.dat.get(), bInfo.nbricks * bOutStorage.step * sizeof(bElem),
+                       cudaMemcpyDeviceToHost));
+  // copy data back into array
+  copyFromBrick<DIM>({EXTENT}, std::vector<long>(DIM, 0), std::vector<long>(DIM, 0), out_arr, grid_ptr, bOut);
+
+  // sanity check: inverse should match in_arr (up to scaling)
+  nvtxRangePushA("inverse_fft_check");
+  {
+    // malloc space for the inverse computation
+    bComplexElem *out_check_arr = zeroComplexArray({EXTENT});
+    bCuComplexElem *out_check_arr_dev;
+    size_t size = NUM_ELEMENTS * sizeof(bComplexElem);
+    cudaCheck(cudaMalloc(&out_check_arr_dev, size));
+    // perform the inverse computation
+    int inverse_direction = (direction == CUFFT_FORWARD) ? FFTPlanType::BRICKS_FFT_INVERSE : FFTPlanType::BRICKS_FFT_FORWARD;
+    cudaCheck(cudaMemcpy(bInStorage_dev.dat.get(), bOutStorage_dev.dat.get(), bInfo.nbricks * bOutStorage.step * sizeof(bElem),
+                         cudaMemcpyDeviceToDevice));
+    compute_fft(inverse_direction);
+    cudaCheck(cudaDeviceSynchronize());
+    cudaCheck(cudaPeekAtLastError());
+    // copy data back from device
+    cudaCheck(cudaMemcpy(bOutStorage.dat.get(), bOutStorage_dev.dat.get(), bInfo.nbricks * bOutStorage.step * sizeof(bElem),
+                        cudaMemcpyDeviceToHost));
+    // copy data back into check-array
+    copyFromBrick<DIM>({EXTENT}, std::vector<long>(DIM, 0), std::vector<long>(DIM, 0), out_check_arr, grid_ptr, bOut);
+    check_close(in_arr, out_check_arr, NUM_ELEMENTS, "cufft 1d j brick transpose inverse check failed", 1.0 / EXTENT_j);
+    // free memroy
+    cudaCheck(cudaFree(out_check_arr_dev));
+    free(out_check_arr);
+  }
+  nvtxRangePop();
+
+  // free memory
+  cudaCheck(cudaFree(grid_ptr_dev));
+  cudaCheck(cudaFree(_bInfo_dev.adj));
+  cudaCheck(cudaFree(bInfo_dev));
+  cudaCheck(cudaFree(_bInfoTransposed_dev.adj));
+  cudaCheck(cudaFree(bInfoTransposed_dev));
+  cudaCheck(cudaFree(grid_ptr_transposed_dev));
+  free(grid_ptr);
+
+  // return timing
+  return num_seconds;
+}
+
 int main(int argc, char **argv)
 {
   if(argc > 4) throw std::runtime_error("too many arguments");
@@ -388,9 +526,24 @@ int main(int argc, char **argv)
   // time cufft for bricks
   if(run_1d_j_brick)
   {
-    #pragma omp parallal
+    // re-zero out out_arr
+    #pragma omp parallel
     for(unsigned i = 0; i < NUM_ELEMENTS; ++i) out_arr[i] = 0.0;
-    nvtxRangePushA("cufft_1d_j_array");
+    nvtxRangePushA("cufft_1d_j_brick_transpose");
+    double cufft_1d_j_brick_transpose_num_seconds = complex_to_complex_1d_j_fft_brick_transpose(in_arr, out_arr, warmup, iter);
+    std::cout << std::setw(colWidth) << "cufft_1d_j_brick_transpose"
+              << std::setw(colWidth) << cufft_1d_j_brick_transpose_num_seconds
+              << std::endl;
+    nvtxRangePop();
+    // run correctness check
+    if(run_1d_j_array)
+    {
+      check_close(out_check_arr, out_arr, NUM_ELEMENTS, "Mismatch between 1d_j_array and 1d_j_brick_transpose");
+    }
+    // re-zero out out_arr
+    #pragma omp parallel
+    for(unsigned i = 0; i < NUM_ELEMENTS; ++i) out_arr[i] = 0.0;
+    nvtxRangePushA("cufft_1d_j_brick");
     double cufft_1d_j_brick_num_seconds = complex_to_complex_1d_j_fft_brick(in_arr, out_arr, warmup, iter);
     std::cout << std::setw(colWidth) << "cufft_1d_j_brick "
               << std::setw(colWidth) << cufft_1d_j_brick_num_seconds
--- single/transpose-cu.h
+++ single/transpose-cu.h
@@ -32,24 +32,22 @@ size_t flatten_index(size_t k, size_t j, size_t i, size_t J, size_t I)
 /**
  * @brief transpose the i-j dimensions of in_mat and store in out_mat
  * 
- * Each thread-block process blockDim.z tiles of size [TileJDim][TileIDim]
+ * 1-dimensional kernel
  * 
  * @tparam elemType the type of elements in the matrix
  * @tparam TileJDim the j-extent of the shared-memory tiles used 
  * @tparam TileIDim the i-extent of the shared-memory tiles used 
- * @param[in] in_mat pointer to array of shape [gridDim.z][J][I]
- * @param[out] out_mat pointer to array of shape [gridDim.z][I][J]
+ * @param[in] in_mat pointer to array of shape [gridDim.z*blockDim.z][J][I]
+ * @param[out] out_mat pointer to array of shape [gridDim.z*blockDim.z][I][J]
+ * @param[in] K number of matrices
  * @param[in] J an extent of the matrices
  * @param[in] I an extent of the matrices
  */
 // Based on https://developer.nvidia.com/blog/efficient-matrix-transpose-cuda-cc/
 template<unsigned TileJDim, unsigned TileIDim, typename elemType>
 __global__
-void transpose_ij(const elemType * __restrict__ in_mat, elemType * __restrict__ out_mat, const size_t J, const size_t I)
+void transpose_ij(const elemType * __restrict__ in_mat, elemType * __restrict__ out_mat, const size_t K, const size_t J, const size_t I)
 {
-  assert(TileJDim * gridDim.y >= J);
-  assert(TileIDim * gridDim.x >= I);
-
   // avoid bank conflicts
   static constexpr unsigned BANK_SIZE = 32;
   static constexpr unsigned BANK_WIDTH_IN_BYTES = 32 / 4;
@@ -58,118 +56,158 @@ void transpose_ij(const elemType * __restrict__ in_mat, elemType * __restrict__ 
   // shift so that bank-shift of one row is one elemnt 
   static constexpr unsigned BANK_SHIFT = (sizeof(elemType) / BANK_WIDTH_IN_BYTES - BANK_OF_TileJDim + BANK_SIZE) % BANK_SIZE; 
   __shared__ elemType tile[TileJDim][TileIDim + BANK_SHIFT * BANK_WIDTH_IN_BYTES / sizeof(elemType)];
-  // find tile
-  size_t k = blockDim.z * blockIdx.z + threadIdx.z;
-  const size_t tile_j_start_idx = min(((size_t) TileJDim) * blockIdx.y, J);
-  const size_t tile_i_start_idx = min(((size_t) TileIDim) * blockIdx.x, I);
-
-  // get upper bounds on tile
-  const size_t upper_bound_j = min((size_t) TileJDim, J - tile_j_start_idx),
-               upper_bound_i = min((size_t) TileIDim, I - tile_i_start_idx);
-
-  // load tile into shared memory
-  for(size_t j = threadIdx.y; j < upper_bound_j; j += blockDim.y)
+  // set up grid-stride loop over tiles
+  const size_t num_tiles_in_i = (I + TileIDim - 1) / TileIDim;
+  const size_t num_tiles_in_j = (J + TileJDim - 1) / TileJDim;
+  const size_t num_tiles = num_tiles_in_i * num_tiles_in_j * K;
+  for(size_t tile_idx = blockIdx.x ; tile_idx < num_tiles; tile_idx += gridDim.x)
   {
-    for(size_t i = threadIdx.x; i < upper_bound_i; i += blockDim.x)
+    size_t tile_i_idx = tile_idx % num_tiles_in_i;
+    size_t tile_j_idx = (tile_idx / num_tiles_in_i) % num_tiles_in_j;
+    size_t k = tile_idx / num_tiles_in_i / num_tiles_in_j;
+    assert(k < K);
+
+    size_t tile_i_offset = min(tile_i_idx * TileIDim, I);
+    size_t tile_j_offset = min(tile_j_idx * TileJDim, J);
+
+    // get upper bounds on tile
+    const size_t upper_bound_j = min((size_t) TileJDim, J - tile_j_offset),
+                upper_bound_i = min((size_t) TileIDim, I - tile_i_offset);
+    const size_t tile_size = upper_bound_j * upper_bound_i;
+    
+    // load tile into shared memory
+    for(unsigned flat_idx = threadIdx.x ; flat_idx < tile_size; flat_idx += blockDim.x)
     {
-      size_t index = flatten_index(k, tile_j_start_idx + j, tile_i_start_idx + i, J, I);
-      assert(index < gridDim.z * blockDim.z * J * I);
+      unsigned i = flat_idx % upper_bound_i;
+      unsigned j = flat_idx / upper_bound_i;
+      size_t index = flatten_index(k, tile_j_offset + j, tile_i_offset + i, J, I);
+      assert(j < upper_bound_j);
+      assert(index < K * J * I);
       tile[j][i] = in_mat[index];
     }
-  }
-  __syncthreads();
-  // store transposed tile
-  for(size_t i = threadIdx.y; i < upper_bound_i; i += blockDim.y)
-  {
-    for(size_t j = threadIdx.x; j < upper_bound_j; j += blockDim.x)
+    __syncthreads();
+    // load tile into shared memory
+    for(unsigned flat_idx = threadIdx.x ; flat_idx < tile_size; flat_idx += blockDim.x)
     {
-      size_t index = flatten_index(k, tile_i_start_idx + i, tile_j_start_idx + j, I, J);
-      assert(index < gridDim.z * blockDim.z * J * I);
+      unsigned j = flat_idx % upper_bound_j;
+      unsigned i = flat_idx / upper_bound_j;
+      size_t index = flatten_index(k, tile_i_offset + i, tile_j_offset + j, I, J);
+      assert(i < upper_bound_i);
+      assert(index < K * J * I);
       out_mat[index] = tile[j][i];
     }
+    __syncthreads();
   }
 }
-
-namespace // anonymous namespace
-{
-  /**
-   * @brief gcd from https://www.geeksforgeeks.org/euclidean-algorithms-basic-and-extended/
-   * std::gcd not included until C++17
-   */
-  int gcd(int a, int b)
-  {
-      if (a == 0)
-          return b;
-      return gcd(b % a, a);
-  }
-
-  /**
-   * @brief transpose in-brick into out-brick
-   * 
-   * @tparam BrickType 
-   * @param[in] in_brick the brick
-   * @param[out] out_brick the brick to transpose into
-   */
-  template<typename BrickType>
-  __global__
-  void transpose_brick_ij(const BrickType &in_brick, BrickType *out_brick)
-  {
-    constexpr unsigned BDIM_j = BrickType::template getBrickDim<1>(),
-                       BDIM_i = BrickType::template getBrickDim<0>();
-    
-    // transpose data
-    for(unsigned flat_idx = threadIdx.x; flat_idx < BDIM_j * BDIM_i; flat_idx += blockDim.x)
-    {
-      unsigned i = flat_idx % BDIM_i;
-      unsigned j = (i / BDIM_i) % BDIM_j;
-      unsigned index = blockDim.x * out_brick->step + BDIM_i * j + i;
-      unsigned index_tr = index + (BDIM_j - 1) * i
-                                + (1 - BDIM_i) * j;
-      unsigned val = out_brick->dat[index];
-      out_brick->dat[index_tr] = val;
-    }
-    // transpose adjacency list
-    if(BrickType::myBrickInfo::adjListIncludesDim(0) && BrickType::myBrickInfo::adjListIncludesDim(1))
-    {
-      for(unsigned i = threadIdx.x; i < static_power<3, BrickType::myBrickInfo::numDimsWithRecordedNeighbors>::value; i += blockDim.x)
-      {
-        unsigned first_digit = i % 3;
-        unsigned second_digit = (i / 3) % 3;
-        unsigned transposed_i = i + 2 * first_digit - 2 * second_digit;
-        out_brick->bInfo->adj[transposed_i] = in_brick.bInfo->adj[i];
-      }
-    }
-  }
-} // end anonymous namespace
 
 /**
- * @brief transpose i-j dimenions of grid and brick
+ * @brief transpose in-brick into out-brick
  * 
- * Transpose i-j dimensions of a grid and associated brick on
- * a CUDA device
+ * Only modifies the data in out_brick's associated BrickStorage. Does
+ * not transpose meta-data.
  * 
- * @tparam TileJDim the j-dimension of the tiles to use when transposing the grid
- * @tparam TileIDim the i-dimension of the tiles to use when transposing the grid
- * @tparam BrickType type of the brick
- * @param[in] in_grid_ptr pointer to in-grid
- * @param[in] in_brick input brick
- * @param[out] out_grid_ptr pointer to output grid
- * @param[out] out_brick pointer to output brick
- * @param[in] grid_size extent of grid in each dimension
+ * Should be invoked as a 1-dimensional kernel
+ * 
+ * @tparam BrickType type of in brick
+ * @tparam BrickTypeTransposed type of out brick
+ * @param[in] in_brick to the brick
+ * @param[out] out_brick to the brick to transpose into
  */
-template<unsigned TileJDim, unsigned TileIDim, typename BrickType>
-void transpose_brick_ij_on_dev(const unsigned *in_grid_ptr, const BrickType &in_brick,
-                               unsigned *out_grid_ptr, BrickType *out_brick,
-                               const std::vector<size_t> grid_size)
+template<typename BrickType, typename BrickTypeTransposed>
+__global__
+void transpose_brick_ij(const BrickType in_brick, BrickTypeTransposed out_brick)
 {
-  // launch transpose of grid
-  constexpr size_t BLOCK_SIZE = 256;
-  const size_t grid_x = (grid_size[0] + TileIDim - 1) / TileIDim;
-  const size_t grid_y = (grid_size[1] + TileJDim - 1) / TileJDim;
-  const size_t collapsed_dims = std::accumulate(grid_size.begin() + 2, grid_size.end(), 1);
-  size_t tilesPerBlock = gcd(BLOCK_SIZE / TileIDim / TileJDim, collapsed_dims);
-  const dim3 grid(grid_x, grid_y, collapsed_dims / tilesPerBlock),
-            block(TileIDim, TileJDim, tilesPerBlock);
-  transpose_ij<TileJDim, TileIDim><< <grid, block>> >(in_grid_ptr, out_grid_ptr, grid_size[1], grid_size[0]);
-  transpose_brick_ij<< <in_brick.bInfo->nbricks, 64>> >(in_brick, out_brick);
+  constexpr unsigned BDIM_j = BrickType::template getBrickDim<1>(),
+                     BDIM_i = BrickType::template getBrickDim<0>(),
+                     VFOLD_j = BrickType::template getVectorFold<1>(),
+                     VFOLD_i = BrickType::template getVectorFold<0>();
+  static_assert(BDIM_i == BrickTypeTransposed::template getBrickDim<1>());
+  static_assert(BDIM_j == BrickTypeTransposed::template getBrickDim<0>());
+  // grid-strided loop over bricks
+  for(unsigned brick_idx = blockIdx.x ; brick_idx < in_brick.bInfo->nbricks; brick_idx += gridDim.x)
+  {
+    // transpose data
+    for(unsigned flat_idx = threadIdx.x; flat_idx < BrickType::BRICKLEN; flat_idx += blockDim.x)
+    {
+      unsigned in_vec = flat_idx % BrickType::VECLEN;
+      unsigned of_vec = flat_idx / BrickType::VECLEN;
+      unsigned i_in_vec = in_vec % VFOLD_i;
+      unsigned j_in_vec = (in_vec / VFOLD_i) % VFOLD_j;
+      unsigned i_of_vec = of_vec % (BDIM_i / VFOLD_i);
+      unsigned j_of_vec = (of_vec / (BDIM_i / VFOLD_i)) % (BDIM_j / VFOLD_j);
+      unsigned new_in_vec_idx = in_vec + (VFOLD_j - 1) * i_in_vec
+                                       + (1 - VFOLD_i) * j_in_vec;
+      unsigned new_of_vec_idx = of_vec + (BDIM_j / VFOLD_j - 1) * i_of_vec
+                                       + (1 - BDIM_i / VFOLD_i) * j_of_vec;
+      assert(new_in_vec_idx < BrickType::VECLEN);
+      assert(new_of_vec_idx < BrickType::BRICKLEN / BrickType::VECLEN);
+      unsigned new_in_brick_idx = new_of_vec_idx * BrickType::VECLEN + new_in_vec_idx;
+
+      unsigned index = flat_idx + in_brick.step * brick_idx;
+      unsigned index_tr = new_in_brick_idx + out_brick.step * brick_idx;
+      assert(index < in_brick.bInfo->nbricks * in_brick.step);
+      assert(index_tr < out_brick.bInfo->nbricks * out_brick.step);
+      typename BrickType::elemType val = in_brick.dat[index];
+      out_brick.dat[index_tr] = val;
+    }
+  } 
 }
+
+/**
+ * @brief transpose in-brick info into out-brick info
+ * 
+ * Should be invoked as a 1-dimensional kernel
+ * 
+ * @tparam BrickInfoType must include neighbors in dimensions 0 and 1.
+ * @param[in] in_brick_info the input brick info
+ * @param[out] out_brick_info pointer to the brick-info to transpose into
+ */
+template<typename BrickInfoType>
+__global__
+void transpose_brick_info_ij(const BrickInfoType *in_brick_info, BrickInfoType *out_brick_info)
+{
+  static_assert(BrickInfoType::adjListIncludesDim(0) && BrickInfoType::adjListIncludesDim(1),
+                "i-j metadata transposition is a nop unless BrickInfoType communicates in dimensions 0 and 1");
+
+  // grid-strided loop over bricks
+  for(unsigned brick_idx = blockIdx.x ; brick_idx < in_brick_info->nbricks; brick_idx += gridDim.x)
+  {
+    constexpr unsigned nneighbors = static_power<3, BrickInfoType::numDimsWithRecordedNeighbors>::value;
+    // transpose data
+    for(size_t idx = threadIdx.x; idx < nneighbors; idx += blockDim.x)
+    {
+      unsigned neighbor = in_brick_info->adj[idx];
+      unsigned first_ternary_digit = idx % 3;
+      unsigned second_ternary_digit = (idx / 3) % 3;
+      unsigned idx_with_flipped_ternary_digits = idx + 2 * first_ternary_digit - 2 * second_ternary_digit;
+      out_brick_info->adj[idx_with_flipped_ternary_digits] = neighbor;
+    }
+  } 
+}
+
+/**
+ * @brief invoke cuda-kernel to transpose i-j dimenions.
+ * 
+ * Case where adjacency list includes neighbors in i and j dimensions
+ * 
+ * @param in_brick_info[in] pointer to input brick-info (on device)
+ * @param out_brick_info[out] pointer to output brick-info (on device)
+ * @see transpose_brick_info_ij
+ */
+template<typename BrickInfoType>
+typename std::enable_if<BrickInfoType::adjListIncludesDim(0) && BrickInfoType::adjListIncludesDim(1)>::type
+transpose_brick_info_ij_on_device(unsigned gridSize, unsigned blockSize, const BrickInfoType *in_brick_info, BrickInfoType *out_brick_info)
+{
+  transpose_brick_info_ij<< <gridSize, blockSize>> >(in_brick_info, out_brick_info);
+}
+
+/**
+ * @brief nop case of transpose in i-j dimensions.
+ * 
+ * @param in_brick_info[in] pointer to input brick-info (on device)
+ * @param out_brick_info[out] pointer to output brick-info (on device)
+ * @see transpose_brick_info_ij
+ */
+template<typename BrickInfoType>
+typename std::enable_if<!(BrickInfoType::adjListIncludesDim(0) && BrickInfoType::adjListIncludesDim(1))>::type
+transpose_brick_info_ij_on_device(unsigned gridSize, unsigned blockSize, const BrickInfoType *in_brick_info, BrickInfoType *out_brick_info) { }
