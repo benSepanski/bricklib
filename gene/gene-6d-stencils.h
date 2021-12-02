574318d3c5e9a399c6104636cbba3112e0f95963
Benjamin Sepanski <ben_sepanski@utexas.edu>
2021-12-02 16:41:34 -0600

    Initial stab at making prettier directory

---
+++ gene/gene-6d-stencils.h
@@ -0,0 +1,126 @@
+//
+// Created by Benjamin Sepanski on 12/2/21.
+//
+
+#ifndef BRICK_GENE_6D_STENCILS_H
+#define BRICK_GENE_6D_STENCILS_H
+
+#include <iomanip>
+#include <iostream>
+
+#include "Array.h"
+#include "BrickedArray.h"
+#include "array-mpi.h"
+#include "brick-cuda.h"
+#include "brickcompare.h"
+#include "bricksetup.h"
+#include "multiarray.h"
+
+// useful constants
+constexpr unsigned RANK = 6;
+constexpr std::array<unsigned, RANK> BRICK_DIM = {2, 16, 2, 2, 1, 1};
+constexpr std::array<unsigned, RANK> ARAKAWA_COEFF_BRICK_DIM = {
+    1, BRICK_DIM[0], BRICK_DIM[2], BRICK_DIM[3], BRICK_DIM[4], BRICK_DIM[5]};
+constexpr unsigned NUM_GHOST_ZONES = 1;
+constexpr std::array<unsigned, RANK> GHOST_ZONE = {2 * NUM_GHOST_ZONES, 0, 2 * NUM_GHOST_ZONES,
+                                                   2 * NUM_GHOST_ZONES, 0, 0};
+constexpr std::array<unsigned, RANK> PADDING = {
+    GHOST_ZONE[0] > 0 ? 2 : 0, GHOST_ZONE[1] > 0 ? 2 : 0, GHOST_ZONE[2] > 0 ? 2 : 0,
+    GHOST_ZONE[3] > 0 ? 2 : 0, GHOST_ZONE[4] > 0 ? 2 : 0, GHOST_ZONE[5] > 0 ? 2 : 0};
+constexpr unsigned TILE_SIZE = 8;
+constexpr unsigned ARAKAWA_STENCIL_SIZE = 13;
+constexpr unsigned NUM_ELEMENTS_PER_FIELD_BRICK =
+    BRICK_DIM[0] * BRICK_DIM[1] * BRICK_DIM[2] * BRICK_DIM[3] * BRICK_DIM[4] * BRICK_DIM[5];
+
+// check constants
+static_assert(GHOST_ZONE[0] % BRICK_DIM[0] == 0, "GHOST_ZONE must be a multiple of BRICK_DIM");
+static_assert(GHOST_ZONE[1] % BRICK_DIM[1] == 0, "GHOST_ZONE must be a multiple of BRICK_DIM");
+static_assert(GHOST_ZONE[2] % BRICK_DIM[2] == 0, "GHOST_ZONE must be a multiple of BRICK_DIM");
+static_assert(GHOST_ZONE[3] % BRICK_DIM[3] == 0, "GHOST_ZONE must be a multiple of BRICK_DIM");
+static_assert(GHOST_ZONE[4] % BRICK_DIM[4] == 0, "GHOST_ZONE must be a multiple of BRICK_DIM");
+static_assert(GHOST_ZONE[5] % BRICK_DIM[5] == 0, "GHOST_ZONE must be a multiple of BRICK_DIM");
+
+// useful types
+typedef Dim<BRICK_DIM[5], BRICK_DIM[4], BRICK_DIM[3], BRICK_DIM[2], BRICK_DIM[1], BRICK_DIM[0]>
+    FieldBrickDimsType;
+typedef Dim<BRICK_DIM[5], BRICK_DIM[4], BRICK_DIM[3], BRICK_DIM[2], BRICK_DIM[0]>
+    PreCoeffBrickDimsType;
+typedef Dim<ARAKAWA_COEFF_BRICK_DIM[5], ARAKAWA_COEFF_BRICK_DIM[4], ARAKAWA_COEFF_BRICK_DIM[3],
+            ARAKAWA_COEFF_BRICK_DIM[2], ARAKAWA_COEFF_BRICK_DIM[1], ARAKAWA_COEFF_BRICK_DIM[0]>
+    ArakawaCoeffBrickDimsType;
+typedef Dim<1> VectorFoldType;
+typedef CommDims<false, false, false, false, false, true> CommIn_i;
+typedef CommDims<false, false, true, true, false, false> CommIn_kl;
+typedef CommDims<false, false, false, false, false, false> NoComm;
+typedef Brick<FieldBrickDimsType, VectorFoldType, true, CommIn_i> FieldBrick_i;
+typedef Brick<FieldBrickDimsType, VectorFoldType, true, CommIn_kl> FieldBrick_kl;
+typedef Brick<PreCoeffBrickDimsType, Dim<1>, true, NoComm> PreCoeffBrick;
+typedef Brick<ArakawaCoeffBrickDimsType, VectorFoldType, false, NoComm> ArakawaCoeffBrick;
+
+typedef brick::Padding<PADDING[5], PADDING[4], PADDING[3], PADDING[2], PADDING[1], PADDING[0]>
+    Padding_kl6D;
+typedef brick::Array<bComplexElem, 6, Padding_kl6D> complexArray6D;
+typedef brick::Array<bElem, 6> realArray6D;
+typedef brick::BrickedArray<bComplexElem, FieldBrickDimsType, VectorFoldType> BrickedFieldArray;
+typedef brick::BrickedArray<bElem, ArakawaCoeffBrickDimsType, VectorFoldType>
+    BrickedArakawaCoeffArray;
+
+// some CUDA constants
+constexpr unsigned WARP_SIZE = 32;
+#if __CUDA_ARCH__ <= 370 || __CUDA_ARCH__ == 750 || __CUDA_ARCH__ == 860
+constexpr unsigned MAX_BLOCKS_PER_SM = 16;
+#else
+constexpr unsigned MAX_BLOCKS_PER_SM = 32;
+#endif
+#if __CUDA_ARCH__ <= 720 || __CUDA_ARCH__ == 800
+constexpr unsigned MAX_WARPS_PER_SM = 64;
+#elif __CUDA_ARCH__ == 750
+constexpr unsigned MAX_WARPS_PER_SM = 32;
+#elif __CUDA__ == 860
+constexpr unsigned MAX_WARPS_PER_SM = 48;
+#else
+#error Unexpected compute capability #__CUDA_ARCH__
+#endif
+
+constexpr unsigned max_blocks_per_sm(unsigned max_block_size) {
+  unsigned max_num_warps_per_block = max_block_size / WARP_SIZE;
+  unsigned max_blocks_per_sm = MAX_WARPS_PER_SM / max_num_warps_per_block;
+  if (max_blocks_per_sm > MAX_BLOCKS_PER_SM) {
+    max_blocks_per_sm = MAX_BLOCKS_PER_SM;
+  }
+  return max_blocks_per_sm;
+}
+
+// used to copy i derivative coefficients into constant memory
+void copy_i_deriv_coeff(const bElem i_deriv_coeff_host[5]);
+
+// used to copy the iteration order of the grid (for the semi-arakawa kernel (vec))
+void copy_grid_iteration_order(const char * grid_iteration_order_host);
+
+
+// declare cuda kernels
+/**
+ * @brief Compute on the non-ghost bricks
+ *
+ * Assumes that grid-size is I x J x KLMN.
+ * Assumes i-deriv coeff has been copied to constant memory
+ * @see copy_i_deriv_coeff
+ */
+__global__ void
+ij_deriv_brick_kernel(brick::Array<unsigned, RANK, brick::Padding<>, unsigned> grid,
+                      brick::Array<unsigned, RANK, brick::Padding<>, unsigned> coeffGrid,
+                      FieldBrick_i bIn, FieldBrick_i bOut, PreCoeffBrick bP1, PreCoeffBrick bP2,
+                      bComplexElem *ikj);
+
+/**
+ * @brief Compute on the non-ghost bricks
+ *
+ * Assumes that grid-size is determined by grid-iteration order
+ * @see copy_grid_iteration_order
+ */
+__global__ void
+semi_arakawa_brick_kernel(brick::Array<unsigned, RANK, brick::Padding<>, unsigned> grid,
+                          brick::Array<unsigned, RANK, brick::Padding<>, unsigned> coeffGrid,
+                          FieldBrick_kl bIn, FieldBrick_kl bOut, ArakawaCoeffBrick coeff);
+
+#endif // BRICK_GENE_6D_STENCILS_H
---
+++ gene/gene6d-gtensor-stencils.h
@@ -0,0 +1,100 @@
+//
+// Created by Benjamin Sepanski on 12/2/21.
+//
+
+#ifndef BRICK_GENE6D_GTENSOR_STENCILS_H
+#define BRICK_GENE6D_GTENSOR_STENCILS_H
+
+#include "gene-6d-stencils.h"
+#define GTENSOR_DEFAULT_DEVICE_ALLOCATOR(T) gt::device_allocator<T>
+#include <gtensor/gtensor.h>
+
+/**
+ * @brief Return a shifted view of the array
+ * copied from
+ * https://github.com/wdmapp/gtensor/blob/41cf4fe26625f8d7ba2d0d3886a54ae6415a2017/benchmarks/bench_hypz.cxx#L14-L24
+ */
+template <int N, typename E> inline auto stencil(E &&e, std::array<int, N> shift) {
+  static_assert(N <= RANK);
+  constexpr int bnd[RANK] = {GHOST_ZONE[0], GHOST_ZONE[1], GHOST_ZONE[2], GHOST_ZONE[3], GHOST_ZONE[4], GHOST_ZONE[5]};
+
+  std::vector<gt::gdesc> slices;
+  slices.reserve(N);
+  for (int d = 0; d < N; d++) {
+    using namespace gt::placeholders;
+    slices.push_back(_s(bnd[d] + shift[d], -bnd[d] + shift[d]));
+  }
+
+  return gt::view<N>(std::forward<E>(e), slices);
+}
+
+/**
+ * @brief Compute the i-j derivative using gtensor (and calls gt::sychronize)
+ *
+ * \fout := p_1 * \frac{\partial}{\partial x} (in_ptr) + p_2 * ikj * in \f$
+ */
+template <typename Space>
+void ij_deriv_gtensor(gt::gtensor<gt::complex<bElem>, 6, Space> in,
+                      gt::gtensor<gt::complex<bElem>, 6, Space> out,
+                      gt::gtensor<gt::complex<bElem>, 5, Space> p1,
+                      gt::gtensor<gt::complex<bElem>, 6, Space> p2,
+                      gt::gtensor<gt::complex<bElem>, 1, Space> ikj,
+                      gt::gtensor<bElem, 1, Space> i_deriv_coeff) {
+  using namespace gt::placeholders;
+  auto _si = _s(GHOST_ZONE[0] + PADDING[0], -GHOST_ZONE[0] - PADDING[0]),
+       _sj = _s(GHOST_ZONE[1] + PADDING[1], -GHOST_ZONE[1] - PADDING[1]),
+       _sk = _s(GHOST_ZONE[2] + PADDING[2], -GHOST_ZONE[2] - PADDING[2]),
+       _sl = _s(GHOST_ZONE[3] + PADDING[3], -GHOST_ZONE[3] - PADDING[3]),
+       _sm = _s(GHOST_ZONE[4] + PADDING[4], -GHOST_ZONE[4] - PADDING[4]),
+       _sn = _s(GHOST_ZONE[5] + PADDING[5], -GHOST_ZONE[5] - PADDING[5]);
+  out.view(_si, _sj, _sk, _sl, _sm, _sn) =
+      p1.view(_all, _newaxis, _all, _all, _all, _all) *
+          (i_deriv_coeff(0) * stencil<RANK>(in, {-2, 0, 0, 0, 0, 0}) +
+           i_deriv_coeff(1) * stencil<RANK>(in, {-1, 0, 0, 0, 0, 0}) +
+           i_deriv_coeff(2) * stencil<RANK>(in, {0, 0, 0, 0, 0, 0}) +
+           i_deriv_coeff(3) * stencil<RANK>(in, {+1, 0, 0, 0, 0, 0}) +
+           i_deriv_coeff(4) * stencil<RANK>(in, {+2, 0, 0, 0, 0, 0})) +
+      p2.view(_all, _newaxis, _all, _all, _all, _all) *
+          ikj.view(_newaxis, _all, _newaxis, _newaxis, _newaxis, _newaxis) *
+          in.view(_si, _sj, _sk, _sl, _sm, _sn);
+
+  // actually compute the result
+  gt::synchronize();
+}
+
+/**
+ * @brief Compute the k-l 13-point arakawa stencil
+ */
+template <typename Space>
+void arakawa_gtensor(gt::gtensor<gt::complex<bElem>, 6, Space> in,
+                     gt::gtensor<gt::complex<bElem>, 6, Space> out,
+                     gt::gtensor<bElem, 5, Space> arakawaCoeff) {
+  using namespace gt::placeholders;
+  auto _si = _s(GHOST_ZONE[0] + PADDING[0], -GHOST_ZONE[0] - PADDING[0]),
+       _sj = _s(GHOST_ZONE[1] + PADDING[1], -GHOST_ZONE[1] - PADDING[1]),
+       _sk = _s(GHOST_ZONE[2] + PADDING[2], -GHOST_ZONE[2] - PADDING[2]),
+       _sl = _s(GHOST_ZONE[3] + PADDING[3], -GHOST_ZONE[3] - PADDING[3]),
+       _sm = _s(GHOST_ZONE[4] + PADDING[4], -GHOST_ZONE[4] - PADDING[4]),
+       _sn = _s(GHOST_ZONE[5] + PADDING[5], -GHOST_ZONE[5] - PADDING[5]);
+
+  auto coeff = [&](int s) { return arakawaCoeff.view(_all, s, _newaxis, _all, _all, _all, _all); };
+  out.view(_si, _sj, _sk, _sl, _sm, _sn) =
+      coeff( 0) * stencil<RANK>(in, {0, 0, +0, -2, 0, 0}) +
+      coeff( 1) * stencil<RANK>(in, {0, 0, -1, -1, 0, 0}) +
+      coeff( 2) * stencil<RANK>(in, {0, 0, +0, -1, 0, 0}) +
+      coeff( 3) * stencil<RANK>(in, {0, 0, +1, -1, 0, 0}) +
+      coeff( 4) * stencil<RANK>(in, {0, 0, -2, +0, 0, 0}) +
+      coeff( 5) * stencil<RANK>(in, {0, 0, -1, +0, 0, 0}) +
+      coeff( 6) * stencil<RANK>(in, {0, 0, +0, +0, 0, 0}) +
+      coeff( 7) * stencil<RANK>(in, {0, 0, +1, +0, 0, 0}) +
+      coeff( 8) * stencil<RANK>(in, {0, 0, +2, +0, 0, 0}) +
+      coeff( 9) * stencil<RANK>(in, {0, 0, -1, +1, 0, 0}) +
+      coeff(10) * stencil<RANK>(in, {0, 0, +0, +1, 0, 0}) +
+      coeff(11) * stencil<RANK>(in, {0, 0, +1, +1, 0, 0}) +
+      coeff(12) * stencil<RANK>(in, {0, 0, +0, +2, 0, 0});
+
+  // actually compute the result
+  gt::synchronize();
+}
+
+#endif // BRICK_GENE6D_GTENSOR_STENCILS_H
