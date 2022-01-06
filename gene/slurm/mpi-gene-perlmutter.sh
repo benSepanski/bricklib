c9e674c16d22b5705c1eeaf0f66a2ff1a7e9c31e
Benjamin Sepanski <ben_sepanski@utexas.edu>
2022-01-06 12:49:04 -0600

    Remove accnt

--- gene/slurm/mpi-gene-perlmutter.sh
+++ gene/slurm/mpi-gene-perlmutter.sh
@@ -2,14 +2,12 @@
 
 BRICKS_BUILD_DIR=${HOME}/jetbrainsRemotes/bricklib/build-perlmutter
 MACHINE=perlmutter
-ACCOUNT=m2956
-EMAIL=ben.sepanski@gmail.com
+EMAIL= # FILL ME IN
 
 for num_gpus in 4 8 12 16 ; do
     python3 mpi-gene.py ${num_gpus} \
         --bricks-build-dir ${BRICKS_BUILD_DIR} \
         -M ${MACHINE} \
-        -A ${ACCOUNT} \
         -e ${EMAIL} \
         > mpi-gene-perlmutter-${num_gpus}gpus.sh
 done
