76bc264b8dc744b66a32b5f4dca578dff1ef50c5
Benjamin Sepanski <ben_sepanski@utexas.edu>
2022-03-12 16:06:45 -0600

    Move shell logic into python

--- gene/slurm/mpi_slurm_gen.py
+++ gene/slurm/mpi_slurm_gen.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     known_machines = [machine for machine in machine_configurations]
     parser = argparse.ArgumentParser("Build slurm job file for MPI Gene")
     parser.add_argument("gpus", metavar="G", type=int, help="Number of GPUs")
+    parser.add_argument("brick_shape", metavar="BI,BJ,BK,BL,BM,BN", type=str, help="Brick shape")
     parser.add_argument("-J", "--job_name", type=str, help="Job name", default=None)
     parser.add_argument('-M', "--machine", type=str, help=f"Machine name, one of {known_machines}")
     parser.add_argument("-e", "--email", type=str, help="Email address to notify on job completion")
     parser.add_argument("-A", "--account", type=str, help="slurm account to charge")
     parser.add_argument("-t", "--time-limit", type=str, help="Time limit, in format for slurm", default="01:00:00")
-    parser.add_argument("--bricks-build-dir", type=str, help="Build directory to use for bricks", default=".")
     parser.add_argument("--compiler", type=str, help="Compiler being used for affinity check", default="gnu")
     parser.add_argument("-d", "--per-process-domain-size", type=str, 
                         help="Per-process domain extent formatted as I,J,K,L,M,N",
@@ -36,6 +36,16 @@ if __name__ == "__main__":
     account_name = args["account"]
     email_address = args["email"]
     compiler = args["compiler"]
+    brick_shape = tuple(map(int, args["brick_shape"].split(",")))
+    assert len(brick_shape) == 6
+    assert all([b > 0 for b in brick_shape])
+
+    vec_len = 1
+    vec_shape = [None for _ in range(len(brick_shape))]
+    for i in range(len(brick_shape)):
+        vec_shape[i] = max(min(brick_shape[i], 32 // vec_len), 1)
+        vec_len *= vec_shape[i]
+    assert vec_len == 32
 
     preamble = build_slurm_gpu_preamble(machine_config, num_gpus, job_name,
                                         email_address=email_address,
@@ -45,13 +55,36 @@ if __name__ == "__main__":
     gtensor_dir = os.getenv("gtensor_DIR")
     if gtensor_dir is None:
         raise ValueError("Environment variable gtensor_DIR must be set")
-    bricks_dir = args["bricks_build_dir"]
 
-    environment_setup = f"""#https://docs.nersc.gov/jobs/affinity/#openmp-environment-variables
+    build_dir = f"cmake-builds/mpi/{machine_config.name}"
+    preamble += f"""
+if [[ ! -f "{build_dir}" ]] ; then
+    mkdir -p "{build_dir}"
+fi
+"""
+
+    def build_job(brick_dim, vec_dim, use_types: bool, cuda_aware: bool):
+        return f"""echo "Building brick-dim {brick_dim} with vec dim {vec_dim}" ; 
+    cmake -S ../../ \\
+        -B {build_dir} \\
+        -DCMAKE_CUDA_ARCHITECTURES={machine_config.cuda_arch} \\
+        -DCMAKE_INSTALL_PREFIX=bin \\
+        -DGENE6D_USE_TYPES={"ON" if use_types else "OFF"} \\
+        -DGENE6D_CUDA_AWARE={"ON" if cuda_aware else "OFF"} \\
+        -DGENE6D_BRICK_DIM={','.join(map(str,reversed(brick_dim)))} \\
+        -DGENE6D_VEC_DIM={','.join(map(str,reversed(vec_dim)))} \\
+        -DCMAKE_CUDA_FLAGS=\"--resource-usage -lineinfo -gencode arch=compute_{machine_config.cuda_arch},code=[sm_{machine_config.cuda_arch},lto_{machine_config.cuda_arch}]\" \\
+        -DCMAKE_BUILD_TYPE=Release \\
+        -DPERLMUTTER={"ON" if machine_config.name == "perlmutter" else "OFF"} \\
+    || exit 1
+    (cd {build_dir} && make clean && make -j 20 mpi-gene6d || exit 1
+"""
+
+    preamble += f"""#https://docs.nersc.gov/jobs/affinity/#openmp-environment-variables
 export OMP_PLACES=threads
 export OMP_PROC_BIND=true
 export gtensor_DIR={gtensor_dir}
-export bricks_DIR={bricks_dir}"""
+"""
 
     num_gz = args["num_gz"]
     output_file = args["output_file"]
@@ -118,6 +151,10 @@ export bricks_DIR={bricks_dir}"""
         + ["\n\n# weak scaling jobs"] + list(map(str, weak_scaling_jobs))
         )
 
-    slurm_script = "\n\n".join([preamble, environment_setup, run_jobs]) + "\n"
+    slurm_scripts = [preamble]
+    for (use_types, cuda_aware) in [(True, False), (False, True), (False, False)]:
+        slurm_scripts += [build_job(brick_shape, vec_shape, use_types, cuda_aware), run_jobs]
+
+    slurm_script = "\n\n".join(slurm_scripts) + "\n"
 
     print(slurm_script)
