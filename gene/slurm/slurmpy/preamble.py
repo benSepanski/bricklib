957a8ec989f2ddc9906703317e7c9acefa8ec09f
Benjamin Sepanski <ben_sepanski@utexas.edu>
2022-03-18 13:18:27 -0500

    Fix cpus per task in slurm gen

--- gene/slurm/slurmpy/preamble.py
+++ gene/slurm/slurmpy/preamble.py
@@ -13,7 +13,9 @@ def build_slurm_gpu_preamble(config, num_gpus, job_name,
                              email_address: str = None,
                              account_name: str = None,
                              time_limit: str = None,
-                             mail_type: Union[MailType, Iterable[MailType]] = None):
+                             mail_type: Union[MailType, Iterable[MailType]] = None,
+                             output_file_name: str = None,
+                             error_file_name: str = None):
     if not isinstance(config, MachineConfig):
         raise TypeError(f"Expected config to be of type {MachineConfig.__class__.__name__}, not {type(config)}")
 
@@ -23,15 +25,23 @@ def build_slurm_gpu_preamble(config, num_gpus, job_name,
         mail_type = [mail_type]
     mail_type = tuple(mail_type)
 
+    if output_file_name is None:
+        output_file_name = f"{job_name}.out"
+    if error_file_name is None:
+        error_file_name = f"{job_name}.err"
+
     min_num_nodes = (num_gpus + config.gpus_per_node - 1) // config.gpus_per_node
     if num_nodes is None:
         num_nodes = min_num_nodes
     if num_nodes < min_num_nodes:
         raise ValueError(f"Need at least {min_num_nodes} nodes for {num_gpus} gpus on {config.name}")
     num_mpi_tasks = num_gpus
-    max_num_cpus_per_task = config.sockets_per_node \
-                            * (config.cores_per_socket // num_mpi_tasks) \
-                            * config.threads_per_core
+    max_num_cpus_per_node = config.sockets_per_node \
+        * config.cores_per_socket \
+        * config.threads_per_core
+    max_num_tasks_per_node = (num_mpi_tasks + num_nodes - 1) // num_nodes
+    max_num_cpus_per_task = max_num_cpus_per_node // max_num_tasks_per_node
+
     if num_cpus_per_task is None:
         num_cpus_per_task = max_num_cpus_per_task
     if num_cpus_per_task > max_num_cpus_per_task:
@@ -47,8 +57,8 @@ def build_slurm_gpu_preamble(config, num_gpus, job_name,
 #SBATCH --gpu-bind single:1
 #SBATCH -q regular
 #SBATCH -J {job_name}
-#SBATCH -o {job_name}.out
-#SBATCH -e {job_name}.err
+#SBATCH -o {output_file_name}
+#SBATCH -e {error_file_name}
 #SBATCH -t {time_limit}"""
     if account_name is not None:
         preamble += f"\n#SBATCH -A {account_name}"
