ebe821e24ee20b6a6d3a2e4d1e7586417b5dcdf0
Benjamin Sepanski <ben_sepanski@utexas.edu>
2022-04-15 14:39:50 -0500

    Give directory to get_ptx_info.py

--- gene/slurm/brick_shape_slurm_gen.py
+++ gene/slurm/brick_shape_slurm_gen.py
@@ -110,7 +110,7 @@ fi
         -DPERLMUTTER={"ON" if machine_config.name == "perlmutter" else "OFF"} \\
     || exit 1
     {shifter_args} "(cd {build_dir} && make clean && make -j 20 single-gene-6d 2> \\"${{ptx_info_file}}\\") || exit 1"
-    {shifter_args} python3 get_ptx_info.py
+    {shifter_args} python3 `pwd`/get_ptx_info.py
 """
 
     def run_job(extent, first_job: bool):
