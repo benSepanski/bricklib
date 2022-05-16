9eebffb5bf5057fbef7393b021cb6930c7e2af67
Benjamin Sepanski <ben_sepanski@utexas.edu>
2022-05-16 18:06:44 -0500

    Add non-transpose option

--- gene/fft-gene-6d.cu
+++ gene/fft-gene-6d.cu
@@ -335,12 +335,13 @@ double complex_to_complex_1d_j_fft_array_callback(bComplexElem *in_arr, bComplex
  * 
  * @param in_arr[in] host-side input array, of rank DIM with extents EXTENT_n,...,EXTENT_i
  * @param out_arr[out] host-side output array of same shape as in_arr
+ * @param performTranspose[in] whether to perform a transpose before/after the FFT.
  * @param direction[in] either CUFFT_FORWARD or CUFFT_INVERSE
  * @param warmup the number of warmups
  * @param iter the number of iters
  * @return the average number of seconds to compute the FFT
  */
-double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComplexElem *out_arr, int warmup, int iter, int direction = CUFFT_FORWARD)
+double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComplexElem *out_arr, int warmup, int iter, bool performTranspose = true, int direction = CUFFT_FORWARD)
 {
   if(direction != CUFFT_FORWARD && direction != CUFFT_INVERSE) throw std::runtime_error("Unrecognized direction");
   // build cufft plan
@@ -367,7 +368,7 @@ double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComple
   gpuCheck(cudaMemcpy(in_arr_dev, in_arr, ARR_SIZE, cudaMemcpyHostToDevice));
 
   // lambda function for timing
-  auto compute_fft = [&plan](const bCuComplexElem *in_arr, bCuComplexElem *intermed_arr, bCuComplexElem *out_arr, int direction) -> void 
+  auto compute_fft = [&plan, performTranspose](bCuComplexElem *in_arr, bCuComplexElem *intermed_arr, bCuComplexElem *out_arr, int direction) -> void
   {
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, 0);
@@ -375,11 +376,16 @@ double complex_to_complex_1d_j_fft_array_transpose(bComplexElem *in_arr, bComple
     int numBlocks = prop.multiProcessorCount * (prop.maxThreadsPerMultiProcessor / blockSize) * 4;
     // transpose in -> out 
     constexpr unsigned TileJ = 32, TileI = 8;
-    transpose_ij<TileJ, TileI><< <numBlocks, blockSize>> >(in_arr, out_arr, NUM_ELEMENTS / EXTENT_j / EXTENT_i, EXTENT_j, EXTENT_i);
+    if(performTranspose) {
+      transpose_ij<TileJ, TileI><< <numBlocks, blockSize>> >(in_arr, out_arr, NUM_ELEMENTS / EXTENT_j / EXTENT_i, EXTENT_j, EXTENT_i);
     // fft out -> intermed
     cufftAlwaysCheck(cufftXtExec(plan, out_arr, intermed_arr, direction));
     // transpose intermed -> out
-    transpose_ij<TileI, TileJ><< <numBlocks, blockSize>> >(intermed_arr, out_arr, NUM_ELEMENTS / EXTENT_j / EXTENT_i, EXTENT_i, EXTENT_j);
+      transpose_ij<TileI, TileJ><<<numBlocks, blockSize>>>(
+          intermed_arr, out_arr, NUM_ELEMENTS / EXTENT_j / EXTENT_i, EXTENT_i, EXTENT_j);
+    } else {
+      cufftAlwaysCheck(cufftXtExec(plan, in_arr, out_arr, direction));
+    }
   };
   // time function (and compute fft in process)
   double num_seconds = cutime_func([&compute_fft, &in_arr_dev, &intermed_arr_dev, &out_arr_dev, &direction]() -> void {
@@ -671,120 +677,11 @@ double complex_to_complex_1d_j_fft_brick_transpose(bComplexElem *in_arr, bComple
   return num_seconds;
 }
 
-/**
- * @brief time computation of 1D FFT over collapsed  i-j-dimensions of in_arr into out_arr
- * 
- * Uses bricks data layout
- * 
- * @param in_arr[in] host-side input array, of rank DIM with extents EXTENT_n,...,EXTENT_i
- * @param out_arr[out] host-side output array of same shape as in_arr
- * @param direction[in] either CUFFT_FORWARD or CUFFT_INVERSE
- * @param warmup number of warmup iterations
- * @param iter number of iterations
- * @return the average number of seconds to compute the FFT
- */
-double complex_to_complex_1d_collaped_ij_fft_brick(bComplexElem *in_arr, bComplexElem *out_arr, int warmup, int iter, int direction = CUFFT_FORWARD)
-{
-  if(direction != CUFFT_FORWARD && direction != CUFFT_INVERSE) throw std::runtime_error("Unrecognized direction");
-
-  // useful typedefs for brick
-  typedef Dim<BDIM_n,BDIM_m,BDIM_l,BDIM_k,BDIM_j * BDIM_i> BrickDims;
-  typedef Dim<1> VFold;
-  constexpr bool isComplex = true;
-  typedef CommDims<false,false,false,false,false> NoComm;
-  typedef Brick<BrickDims, VFold, isComplex, NoComm> ComplexBrick;
-
-  // move arrays to host-side bricks
-  unsigned *grid_ptr = nullptr;
-  const std::vector<long> brick_extent = {(EXTENT_j * BDIM_i) / (BDIM_i * BDIM_j), 
-                                          ((EXTENT_i / BDIM_i) * EXTENT_k) / BDIM_k,
-                                          BRICK_EXTENT_l,
-                                          BRICK_EXTENT_m,
-                                          BRICK_EXTENT_n};
-  BrickInfo<RANK-1, NoComm> bInfo = init_grid<RANK-1, NoComm>(grid_ptr, brick_extent);
-  BrickStorage bInStorage = bInfo.allocate(ComplexBrick::BRICKSIZE),
-               bOutStorage = bInfo.allocate(ComplexBrick::BRICKSIZE);
-  ComplexBrick bIn(&bInfo, bInStorage, 0),
-               bOut(&bInfo, bOutStorage, 0);
-  const std::vector<long> arr_extent = {BDIM_i * EXTENT_j, EXTENT_i / BDIM_i * EXTENT_k, EXTENT_l, EXTENT_m, EXTENT_n};
-  copyToBrick<RANK-1>(arr_extent, std::vector<long>(RANK-1, 0), std::vector<long>(RANK-1, 0), in_arr, grid_ptr, bIn);
-
-  // set up brick info in cuda
-  BrickInfo<RANK-1, NoComm> *bInfo_dev;
-  BrickInfo<RANK-1, NoComm> _bInfo_dev = movBrickInfo(bInfo, cudaMemcpyHostToDevice);
-  gpuCheck(cudaMalloc(&bInfo_dev, sizeof(BrickInfo<RANK-1, NoComm>)));
-  gpuCheck(cudaMemcpy(bInfo_dev, &_bInfo_dev, sizeof(BrickInfo<RANK-1, NoComm>), cudaMemcpyHostToDevice));
-  // mov brick storage to cuda
-  BrickStorage bInStorage_dev = movBrickStorage(bInStorage, cudaMemcpyHostToDevice),
-               bOutStorage_dev = movBrickStorage(bOutStorage, cudaMemcpyHostToDevice);
-  // set up brick in cuda
-  ComplexBrick bIn_dev(bInfo_dev, bInStorage_dev, 0);
-  ComplexBrick bOut_dev(bInfo_dev, bOutStorage_dev, 0);
-  // copy grids to device
-  unsigned *grid_ptr_dev;
-  size_t size = sizeof(unsigned) * NUM_BRICKS;
-  gpuCheck(cudaMalloc(&grid_ptr_dev, size));
-  gpuCheck(cudaMemcpy(grid_ptr_dev, grid_ptr, size, cudaMemcpyHostToDevice));
-
-  // set up FFT for bricks
-  typedef BricksCufftPlan<ComplexBrick, FourierType<ComplexToComplex, 0> > FFTPlanType;
-  std::array<size_t, RANK-1> brick_extent_as_arr;
-  std::copy(brick_extent.begin(), brick_extent.end(), brick_extent_as_arr.data());
-  FFTPlanType plan(brick_extent_as_arr);
-  plan.setup(bIn_dev, grid_ptr_dev, bOut_dev, grid_ptr_dev);
-
-  auto compute_fft = [&plan] (bool direction = FFTPlanType::BRICKS_FFT_FORWARD) -> void { plan.launch(direction); };
-  // time function (and compute fft in process)
-  double num_seconds = cutime_func(compute_fft, warmup, iter);
-
-  // copy data back from device
-  gpuCheck(cudaMemcpy(bOutStorage.dat.get(), bOutStorage_dev.dat.get(), bInfo.nbricks * bOutStorage.step * sizeof(bElem),
-                       cudaMemcpyDeviceToHost));
-  // copy data back into array
-  copyFromBrick<RANK-1>(arr_extent, std::vector<long>(RANK-1, 0), std::vector<long>(RANK-1, 0), out_arr, grid_ptr, bOut);
-
-  // sanity check: inverse should match in_arr (up to scaling)
-  nvtxRangePushA("inverse_fft_check");
-  {
-    // malloc space for the inverse computation
-    bComplexElem *out_check_arr = zeroComplexArray({EXTENT});
-    bCuComplexElem *out_check_arr_dev;
-    size_t size = NUM_ELEMENTS * sizeof(bComplexElem);
-    gpuCheck(cudaMalloc(&out_check_arr_dev, size));
-    // perform the inverse computation
-    int inverse_direction = (direction == CUFFT_FORWARD) ? FFTPlanType::BRICKS_FFT_INVERSE : FFTPlanType::BRICKS_FFT_FORWARD;
-    plan.setup(bOut_dev, grid_ptr_dev, bOut_dev, grid_ptr_dev);
-    compute_fft(inverse_direction);
-    gpuCheck(cudaDeviceSynchronize());
-    gpuCheck(cudaPeekAtLastError());
-    // copy data back from device
-    gpuCheck(cudaMemcpy(bOutStorage.dat.get(), bOutStorage_dev.dat.get(), bInfo.nbricks * bOutStorage.step * sizeof(bElem),
-                        cudaMemcpyDeviceToHost));
-    // copy data back into check-array
-    copyFromBrick<RANK-1>(arr_extent, std::vector<long>(RANK-1, 0), std::vector<long>(RANK-1, 0), out_check_arr, grid_ptr, bOut);
-    check_close(in_arr, out_check_arr, NUM_ELEMENTS, "cufft 1d collaped i-j brick inverse check failed", 1.0 / arr_extent[0]);
-    // free memroy
-    gpuCheck(cudaFree(out_check_arr_dev));
-    free(out_check_arr);
-  }
-  nvtxRangePop();
-
-  // free memory
-  gpuCheck(cudaFree(grid_ptr_dev));
-  gpuCheck(cudaFree(_bInfo_dev.adj));
-  gpuCheck(cudaFree(bInfo_dev));
-  free(grid_ptr);
-
-  // return timing
-  return num_seconds;
-}
-
 int main(int argc, char **argv)
 {
   if(argc > 4) throw std::runtime_error("too many arguments");
   bool run_1d_j_array = true,
-       run_1d_j_brick = true,
-       run_1d_collapsed_ij_brick = false;
+       run_1d_j_brick = true;
   int warmup = CU_WARMUP, iter = CU_ITER;
   if(argc >= 2) warmup = std::stoi(argv[1]);
   if(argc >= 3) iter = std::stoi(argv[2]);
@@ -793,12 +690,7 @@ int main(int argc, char **argv)
     char to_run = argv[3][0];
     if(to_run == 'a') run_1d_j_brick = false;
     else if(to_run == 'b') run_1d_j_array = false;
-    else if(to_run == 'c') {
-      run_1d_j_array = false;
-      run_1d_j_brick = false;
-      run_1d_collapsed_ij_brick = true;
-    }
-    else throw std::runtime_error("Unrecognized argument, expected 'a', 'b', or 'c'");
+    else throw std::runtime_error("Unrecognized argument, expected 'a' or 'b'");
   }
 
   CSVDataRecorder dataRecorder;
@@ -870,6 +762,15 @@ int main(int argc, char **argv)
     dataRecorder.record("callback", "false");
     dataRecorder.record("avgtime(s)", cufft_1d_j_array_transpose_num_seconds);
 
+
+    dataRecorder.newRow();
+    nvtxRangePushA("cufft_1d_j_array_excluding_transpose");
+    double cufft_1d_j_array_excluding_transpose_num_seconds = complex_to_complex_1d_j_fft_array_transpose(in_arr, out_arr, warmup, iter, false);
+    std::cout << std::setw(colWidth) << "cufft_1d_j_array_excluding_transpose "
+              << std::setw(colWidth) << 1000 * cufft_1d_j_array_excluding_transpose_num_seconds
+              << std::endl;
+    nvtxRangePop();
+
     dataRecorder.newRow();
     nvtxRangePushA("cufft_1d_j_array");
     double cufft_1d_j_array_num_seconds = complex_to_complex_1d_j_fft_array(in_arr, out_arr, warmup, iter);
@@ -929,18 +830,6 @@ int main(int argc, char **argv)
     dataRecorder.record("transpose", "false");
     dataRecorder.record("callback", "true");
     dataRecorder.record("avgtime(s)", cufft_1d_j_brick_num_seconds);
-  }
-  if(run_1d_collapsed_ij_brick)
-  {
-    // re-zero out out_arr
-    #pragma omp parallel
-    for(unsigned i = 0; i < NUM_ELEMENTS; ++i) out_arr[i] = 0.0;
-    nvtxRangePushA("cufft_1d_collapsed_ij_brick");
-    double cufft_1d_collapsed_ij_num_seconds = complex_to_complex_1d_collaped_ij_fft_brick(in_arr, out_arr, warmup, iter);
-    std::cout << std::setw(colWidth) << "cufft_1d_collapsed_ij"
-              << std::setw(colWidth) << 1000 * cufft_1d_collapsed_ij_num_seconds
-              << std::endl;
-    nvtxRangePop();
   }
 
   // free memory
